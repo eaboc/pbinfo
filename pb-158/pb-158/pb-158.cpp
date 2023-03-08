537325ea4bb87eee4e7a3501cdcf933e3a62d415
eaboc <122721957+eaboc@users.noreply.github.com>
2023-03-08 07:05:59 +0000

    (PB-158): 100pct

--- pb-158/pb-158/pb-158.cpp
+++ pb-158/pb-158/pb-158.cpp
@@ -7,21 +7,23 @@ int n, p, x, v[25];
 int main()
 {
     cin >> n >> p >> x;
+    
+    for (int i = 0; i < n; i++)
+    {
+        cin >> v[i];
+    }
 
     for (int i = 0; i < n; i++)
     {
-        if (i > p-1)
+        if (n - i >= p)
         {
-            cin >> v[n-i];
-        } else if (i == p-1) {
-            v[n - i] = x;
-        }
-        else {
-            cin >> v[n];
+            v[n-i] = v[n-i-1];
+        } else if (n - i == p - 1) {
+            v[n-i] = x;
         }
     }
 
-    for (int i = 0; i < n - 1; i++)
+    for (int i = 0; i < n + 1; i++)
     {
         cout << v[i] << " ";
     }
