5ec7d69308a5929b6860abb1a1d39f76f6386770
eaboc <122721957+eaboc@users.noreply.github.com>
2023-03-08 12:23:17 +0000

    fix(PB-158): Resolve edit for first element in sequence

--- pb-158/pb-158/pb-158.cpp
+++ pb-158/pb-158/pb-158.cpp
@@ -8,17 +8,19 @@ int main()
 {
     cin >> n >> x >> p;
     
+    p--; //The problem requires the index to start at 1 instead of 0
+    
     for (int i = 0; i < n; i++)
     {
         cin >> v[i];
     }
 
-    for (int i = 0; i < n; i++)
+    for (int i = 0; i < n  + 1; i++)
     {
-        if (n - i >= p)
+        if (n - i > p)
         {
             v[n-i] = v[n-i-1];
-        } else if (n - i == p - 1) {
+        } else if (n - i == p) {
             v[n-i] = x;
         }
     }
